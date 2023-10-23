7ed8df2a34b442314bf4123b7092133b20a69d8e
NekkuDB <129802276+NekkuDB@users.noreply.github.com>
2023-10-23 19:43:45 +0000

    Express Profile Page.AddFcs

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage {
 
   constructor() { }
 
-  ngOnInit() {
+  changePassword() {
+    console.log("Change password clicked");
+    // Implement functionality here
   }
 
+  verifyEmail() {
+    console.log("Verify email clicked");
+    // Implement functionality here
+  }
+
+  changeAddress() {
+    console.log("Change address clicked");
+    // Implement functionality here
+  }
+
+  enable2FA() {
+    console.log("Enable 2FA clicked");
+    // Implement functionality here
+  }
+
+  logout() {
+    console.log("Logout clicked");
+    // Implement functionality here
+  }
 }
