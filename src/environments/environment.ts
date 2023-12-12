701dc6a19eb9338e58427aa9c97b767e99ad3d28
iMrSeba <112730485+iMrSeba@users.noreply.github.com>
2023-12-12 20:42:32 +0000

    URLProduct

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'https://app-service-user3.onrender.com',  
+  apiUrlProduct: 'https://product-services-rgaf.onrender.com',
   firebaseConfig: {
     apiKey: "AIzaSyB_EH7vxA4CGUa1WOerUYRkDrRbwPevSU0",
     authDomain: "imageuser-89a50.firebaseapp.com",
